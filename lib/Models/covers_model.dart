a0df5df673e29209da28d3ed51768eb8670f3a0f
Youssef Mansour <yousaf83000@gmail.com>
2025-07-22 00:02:17 +0300

    create covers model

---
+++ lib/Models/covers_model.dart
@@ -0,0 +1,11 @@
+class CoversModel {
+  final String image;
+  final String title;
+
+  CoversModel({required this.image, required this.title});
+  static List<CoversModel> covers = [
+    CoversModel(image: 'assets/cover/cover2.png', title: 'Ikhtain Collectio'),
+    CoversModel(image: 'assets/cover/cover3.png', title: 'Black collection'),
+    CoversModel(image: 'assets/cover/cover1.png', title: 'Women In Black  '),
+  ];
+}
