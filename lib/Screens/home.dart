2e3fa375345a0ba0007615288a3df1c852b713dc
Youssef Mansour <yousaf83000@gmail.com>
2025-07-22 00:17:05 +0300

    create list of collections

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:open_fashion/Components/custom_text.dart';
+import 'package:open_fashion/Models/covers_model.dart';
 import 'package:open_fashion/Models/product_model.dart';
 import 'package:open_fashion/core/colors.dart';
 import 'package:open_fashion/Components/custom_appbar.dart';
@@ -51,6 +52,8 @@ class _HomeState extends State<Home> {
                   Gap(120),
                   Image.asset('assets/cover/cover1.png'),
                   Gap(20),
+
+                  // GridView for products
                   GridView.builder(
                     padding: EdgeInsets.all(0),
                     shrinkWrap: true,
@@ -90,8 +93,36 @@ class _HomeState extends State<Home> {
                     size: 24,
                   ),
                   Image.asset('assets/svgs/line.png', width: 200),
-                  Gap(20),
 
+                  Gap(40),
+
+                  // ListView for covers
+                  SizedBox(
+                    height: 500,
+                    child: ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      itemCount: CoversModel.covers.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        final item = CoversModel.covers[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(item.image, height: 300),
+                              Gap(8),
+                              CustomText(
+                                text: item.title.toUpperCase(),
+                                size: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                   Gap(200),
                 ],
               ),
