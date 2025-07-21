2d631fb0f3e6b058c3a7c1dcf157228a92cc141e
Youssef Mansour <yousaf83000@gmail.com>
2025-07-21 20:05:57 +0300

    Open Fashion

---
+++ lib/Components/custom_appbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:open_fashion/Components/colors.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppbar({super.key, this.isBlackk});
+  final isBlackk;
+  @override
+  Size get preferredSize => const Size.fromHeight(80);
+
+  @override
+  Widget build(BuildContext context) {
+    bool isBlack = isBlackk;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: AppBar(
+        backgroundColor: isBlack ? AppColors.primary : Colors.white,
+        leadingWidth: 25,
+        leading: SvgPicture.asset(
+          "assets/svgs/Menu.svg",
+          color: isBlack ? Colors.white : AppColors.primary,
+        ),
+        title: SvgPicture.asset(
+          "assets/logo/logo-bg.svg",
+          color: isBlack ? Colors.white : AppColors.primary,
+        ),
+        centerTitle: true,
+        actions: [
+          SvgPicture.asset(
+            "assets/svgs/Search.svg",
+            color: isBlack ? Colors.white : AppColors.primary,
+          ),
+          Gap(20),
+          SvgPicture.asset(
+            "assets/svgs/shopping bag.svg",
+            color: isBlack ? Colors.white : AppColors.primary,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Screens/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:open_fashion/Components/colors.dart';
+import 'package:open_fashion/Components/custom_appbar.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppbar(isBlackk: true),
+      backgroundColor: AppColors.primary,
+    );
+  }
+}
